6532db8957205ef7180a825fe453c82c0c1ba86f
MFG38 <mfg38thelps@aol.com>
2021-03-20 19:46:18 +0200

    New "experiment": python_passwordgen

---
+++ python_passwordgen/passwordgen.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+chars = string.ascii_letters + string.digits
+
+minLength = 8
+maxLength = 33      # 32 + 1, just so that max length doesn't cap at 31.
+
+def genPassword(length):
+    if length not in range(minLength,maxLength):
+        print("Can't generate password: length is out of bounds!")
+    else:
+        generatedPW = ''.join((random.choice(chars) for i in range(length)))
+        print("Your password is: " + generatedPW)
+
+passLength = int(input("Enter desired length of password (8-32 characters): "))
+genPassword(passLength)

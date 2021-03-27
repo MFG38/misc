02375fe38139467ba7f55f36880f9c230f6c057f
MFG38 <mfg38thelps@aol.com>
2021-03-27 16:11:05 +0200

    New experiment: batch_gzdconfigbackup

---
+++ batch_gzdconfigbackup/configbackup.bat
@@ -0,0 +1,26 @@
+@echo off
+
+echo GZDoom Config File Backup Utility
+echo by Santtu "MFG38" Pesonen, 2021
+echo ---------------------------------
+echo Type 'backup' to create a backup of an existing config file.
+echo Type 'restore' to restore a config from a backup.
+echo.
+set /p OPERATION="Which operation do you wish to perform? " % = %
+
+if (%OPERATION%) == (backup) (goto OpBackup)
+if (%OPERATION%) == (restore) (goto OpRestore)
+
+:OpBackup
+set /p INFILE="Enter name of input file: " % = %
+set /p OUTFILE="Enter name of output file: " % = %
+copy %INFILE%.ini %OUTFILE%.ini
+goto OpFinish
+
+:OpRestore
+set /p INFILE="Enter name of input file: " % = %
+copy %INFILE%.ini gzdoom-%USERNAME%.ini
+goto OpFinish
+
+:OpFinish
+pause >nul

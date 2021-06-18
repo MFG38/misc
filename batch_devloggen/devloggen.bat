0845a7f92a1520d3f8a081e15d1a768a79cd6a65
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-06-18 22:03:35 +0300

    batch_devloggen: Update date offsets

--- batch_devloggen/devloggen.bat
+++ batch_devloggen/devloggen.bat
@@ -1,8 +1,8 @@
 @echo off
 
 :: Edit the offsets as needed to suit your system time.
-set logdate=%date:~9,4%%date:~6,2%%date:~3,2%
-set datetext=%date:~9,4%-%date:~6,2%-%date:~3,2%
+set logdate=%date:~6,4%%date:~3,2%%date:~0,2%
+set datetext=%date:~6,4%-%date:~3,2%-%date:~0,2%
 
 echo Development Log > devlog_%logdate%.txt
 echo %datetext% >> devlog_%logdate%.txt

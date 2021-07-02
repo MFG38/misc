fbd849c2eb3520347e324c7d6bcc5f112f2334de
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-07-02 19:11:33 +0300

    Add python_diceroller

---
+++ python_diceroller/diceroller.py
@@ -0,0 +1,15 @@
+import random
+
+min = 1
+max = 6
+
+rollAgain = "y"
+
+while rollAgain == "y":
+    print("Rolling dice...")
+    print("The dice have been rolled. The numbers are:")
+    print(random.randint(min,max))
+    print(random.randint(min,max))
+    
+    rollAgain = input("Roll again? ")
+

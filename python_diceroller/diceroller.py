43912b04cb164ef9d444396f81992cc30d28ed08
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-07-03 03:48:59 +0300

    python_diceroller: roll a single die instead of two

--- python_diceroller/diceroller.py
+++ python_diceroller/diceroller.py
@@ -9,7 +9,6 @@ while rollAgain == "y":
     print("Rolling dice...")
     print("The dice have been rolled. The numbers are:")
     print(random.randint(min,max))
-    print(random.randint(min,max))
     
     rollAgain = input("Roll again? ")
 

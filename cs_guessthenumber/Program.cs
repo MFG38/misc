7346a1b7ac4ed3c66364d4f1e8a3492d22fde5f4
Santtu Pesonen <55315235+MFG38@users.noreply.github.com>
2021-09-04 18:11:23 +0300

    Expand cs_guessthenumber with limited number of guesses and Game Over state

--- cs_guessthenumber/Program.cs
+++ cs_guessthenumber/Program.cs
@@ -8,11 +8,13 @@ namespace GuessTheNumber
         {
             Random r = new Random();
 
+            int guessesLeft = 5;
             int wnum = r.Next(1, 20);
             bool won = false;
 
             do
             {
+                Console.WriteLine("Guesses left: {0}", guessesLeft);
                 Console.Write("Guess a nomber between 1 and 20: ");
                 string gs = Console.ReadLine();
 
@@ -21,17 +23,29 @@ namespace GuessTheNumber
                 if(gi > wnum)
                 {
                     Console.WriteLine("Nope! Gotta go lower!");
+                    guessesLeft--;
+
+                    if(guessesLeft == 0)
+                    {
+                        Console.WriteLine("You ran out of guesses! Game over!");
+                    }
                 }
                 else if(gi < wnum)
                 {
                     Console.WriteLine("Nope! Gotta go higher!");
+                    guessesLeft--;
+
+                    if (guessesLeft == 0)
+                    {
+                        Console.WriteLine("You ran out of guesses! Game over!");
+                    }
                 }
                 else if(gi == wnum)
                 {
                     Console.WriteLine("Oh hey, you guessed the number.");
                     won = true;
                 }
-            } while (won == false);
+            } while ((won == false) && (guessesLeft > 0));
 
             Console.WriteLine("Thanks for playing!");
             Console.Write("Press any key to exit.");

59d60d6df248862710c960755772ca1e23141ebd
MFG38 <santtu.pesonen@mail.com>
2021-10-23 15:49:49 +0300

    python_dnddiceroller: Replace comma with "+" sign

--- python_dnddiceroller/dnddiceroller.py
+++ python_dnddiceroller/dnddiceroller.py
@@ -9,7 +9,7 @@ def roll(roll):
     except Exception as err:
         print(f'Fault encountered: {err}')
     
-    print(f'You rolled {", ".join(str(x) for x in rolling)} for a total of {sum(rolling)}.')
+    print(f'You rolled {"+".join(str(x) for x in rolling)} for a total of {sum(rolling)}.')
 
 while True:
     print("Input your roll in xdy format, where x is the number of dice")

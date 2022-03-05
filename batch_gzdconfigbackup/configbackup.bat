031156111e416ee2261504f68905f02207fe73a3
MFG38 <santtu.pesonen@mail.com>
2022-03-05 11:06:44 +0200

    batch_gzdconfigbackup: update with shorthand op names

--- batch_gzdconfigbackup/configbackup.bat
+++ batch_gzdconfigbackup/configbackup.bat
@@ -3,13 +3,16 @@
 echo GZDoom Config File Backup Utility
 echo by Santtu "MFG38" Pesonen, 2021
 echo ---------------------------------
-echo Type 'backup' to create a backup of an existing config file.
-echo Type 'restore' to restore a config from a backup.
+echo Type 'backup' or 'b' to create a backup of an existing config file.
+echo Type 'restore' or 'r' to restore a config from a backup.
 echo.
 set /p OPERATION="Which operation do you wish to perform? " % = %
 
 if (%OPERATION%) == (backup) (goto OpBackup)
+if (%OPERATION%) == (b) (goto OpBackup)
+
 if (%OPERATION%) == (restore) (goto OpRestore)
+if (%OPERATION%) == (r) (goto OpRestore)
 
 :OpBackup
 set /p INFILE="Enter name of input file: " % = %

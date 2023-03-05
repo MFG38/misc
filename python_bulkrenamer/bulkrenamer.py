c695ba1aa279aab63f130b70f0bf3b892f8d2f42
MFG38 <santtu.pesonen@mail.com>
2023-03-05 12:00:15 +0200

    Add python_bulkrenamer

---
+++ python_bulkrenamer/bulkrenamer.py
@@ -0,0 +1,18 @@
+import os
+import re
+
+wd = os.getcwd()
+
+def renameBulk(path, pattern, replacement):
+    for filename in os.listdir(wd):
+        if re.search(pattern, filename):
+            renameTarget = re.sub(pattern, replacement, filename)
+            newName = os.path.join(path, renameTarget)
+            oldName = os.path.join(path, filename)
+            os.rename(oldName, newName)
+
+while True:
+    print("Working directory is " + format(wd))
+    lookFor = input("Enter pattern to look for: ")
+    replaceWith = input("Enter replacement pattern: ")
+    renameBulk(wd, lookFor, replaceWith)

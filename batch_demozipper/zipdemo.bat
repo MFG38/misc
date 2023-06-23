488354d4e8efd4097ec60aa526e849734575f551
MFG38 <santtu.pesonen@mail.com>
2023-06-23 12:17:03 +0300

    batch_demozipper: allow zipping multiple demos at once

--- batch_demozipper/zipdemo.bat
+++ batch_demozipper/zipdemo.bat
@@ -1,6 +1,10 @@
 @echo off
 
+:BeginZip
 set /p FILE="Name of target zip: " % = %
 :: Modify the line below as needed if your path to 7za.exe is different.
 _tools\7za a -tzip %FILE%.zip %FILE%.lmp %FILE%.txt
-pause >nul
+
+set /p DOMORE="Zip another file? " % = %
+if (%DOMORE%) == (y) (goto BeginZip)
+if (%DOMORE%) == (n) (exit)

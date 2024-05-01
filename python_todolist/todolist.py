61fab3df54314f918db9b74e767de840865cb048
MFG38 <santtu.pesonen@mail.com>
2024-05-01 15:05:53 +0300

    python_todolist: Add exit command + fix rem command error

--- python_todolist/todolist.py
+++ python_todolist/todolist.py
@@ -1,14 +1,14 @@
 todo = []
 
 while True:
-    command = input("Command (add, rem, list, sort): ")
+    command = input("Command (add, rem, list, sort, exit): ")
 
     if command == "add":
         entry = input("Insert todo item: ")
         todo.append(entry)
     elif command == "rem":
         i = int(input("Insert index of item to remove: "))
-        if i in range(0,len(todo)-1):
+        if i in range(0,len(todo)):
             todo.remove(todo[i])
         else:
             print("ERROR: Index out of bounds")
@@ -17,5 +17,7 @@ while True:
             print(str(todo.index(entry)), entry)
     elif command == "sort":
         todo.sort()
+    elif command == "exit":
+        break
     else:
         print("ERROR: Not a recognized command")

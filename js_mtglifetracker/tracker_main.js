fe638711aed9717fb2f040c2e194ad641dee928b
MFG38 <santtu.pesonen@mail.com>
2024-06-28 09:17:55 +0300

    js_mtglifetracker: Fix a memory leak or whatever

--- js_mtglifetracker/tracker_main.js
+++ js_mtglifetracker/tracker_main.js
@@ -2,8 +2,9 @@ let life = 20;
 
 function displayLife() {
 	document.getElementById("trackerPlaceholder").innerText = life;
-	setInterval(displayLife, 50);
 }
+
+setInterval(displayLife, 50);
 
 function lifeUp () {
 	life += 1;
